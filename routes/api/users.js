021c53b14fbd1d0438a16abd72f0ae05fec49edf
doctor_zalman <georgesun30@gmail.com>
2021-12-30 14:03:14 +0200

    fixed problem with proxy and make Request Exam & Register/Login form

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,7 +7,7 @@ const config = require('config');
 const {check, validationResult} = require('express-validator');
 
 const User = require('../../models/User');
-// @route   Post api/users
+// @route   POST api/users
 // @desc    Register User
 // @access  Public
 
