6b4af78420ebf0d049d57c234a5b35d5c745b997
doctor_zalman <georgesun30@gmail.com>
2022-01-06 11:33:29 +0200

    40 auth reducer - register action

--- client/src/store.js
+++ client/src/store.js
@@ -4,9 +4,8 @@ import thunk from "redux-thunk";
 import rootReducer from "./reducers"
 
 const initialState ={};
-
 const middleware = [thunk];
-
+// Create the Redux Store
 const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
 
 export default store;
